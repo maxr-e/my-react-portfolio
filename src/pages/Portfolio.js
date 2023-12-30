1975605f26e06a872bd706bac48c4dfc788ea141
Maxps <maxroyal.eisenberg@gmail.com>
2023-12-30 01:22:18 -0500

    Added portfolio text

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>
+        PORTFOLIO PORTFOLIO PORTFOLIO Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at. PORTFOLIO PORTFOLIO PORTFOLIO 
+      </p>
+    </div>
+  );
+}
